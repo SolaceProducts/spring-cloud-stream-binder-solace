20ba8c6db12b7a433addfdb573f259ab43732ef5
Jeffrey Douangpaseuth <jdouangpaseuth@hotmail.ca>
2018-08-03 10:42:25 -0400

    wrap entire Spring message in MessageWrapper before sending to router

--- src/main/java/com/solace/spring/stream/binder/util/XMLMessageMapper.java
+++ src/main/java/com/solace/spring/stream/binder/util/XMLMessageMapper.java
@@ -7,6 +7,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.integration.support.DefaultMessageBuilderFactory;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.MessagingException;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.util.SerializationUtils;
@@ -23,51 +24,119 @@ public class XMLMessageMapper {
 	private static final String MIME_JAVA_SERIALIZED_OBJECT = "application/x-java-serialized-object";
 
 	public XMLMessage map(Message<?> message) {
+		byte[] messageWrapperBytes = SerializationUtils.serialize(createMessageWrapper(message));
+		BytesXMLMessage xmlMessage = JCSMPFactory.onlyInstance().createMessage(BytesXMLMessage.class);
+		xmlMessage.writeAttachment(messageWrapperBytes);
+		xmlMessage.setHTTPContentType(MIME_JAVA_SERIALIZED_OBJECT);
+		return xmlMessage;
+	}
+
+	public Message<?> map(XMLMessage xmlMessage) throws MessagingException {
+		MessageWrapper messageWrapper = extractMessageWrapper(xmlMessage);
+
+		Object payload = null;
+		byte[] payloadBytes = messageWrapper.getPayload();
+		String mimeType = messageWrapper.getPayloadMimeType();
+
+		if (mimeType.startsWith(MimeTypeUtils.TEXT_PLAIN.getType())) {
+			String encodingName = messageWrapper.getCharset();
+			Charset encoding = StringUtils.hasText(encodingName) ? Charset.forName(encodingName) : DEFAULT_ENCODING;
+			payload = new String(payloadBytes, encoding);
+
+		} else if (mimeType.equalsIgnoreCase(MIME_JAVA_SERIALIZED_OBJECT)) {
+			payload = SerializationUtils.deserialize(payloadBytes);
+		}
+
+		return new DefaultMessageBuilderFactory().withPayload(payload != null ? payload : payloadBytes)
+				.copyHeaders(messageWrapper.getHeaders())
+				.setHeaderIfAbsent("deliveryAttempt", new AtomicInteger(0))
+				.build();
+	}
+
+	private MessageWrapper createMessageWrapper(Message<?> message) {
 		Object payload = message.getPayload();
 		String mimeType;
-		byte[] attachment;
+		byte[] payloadBytes;
 		Charset charset = null;
 
 		if (payload instanceof byte[]) {
 			mimeType = MimeTypeUtils.APPLICATION_OCTET_STREAM_VALUE;
-			attachment = (byte[]) payload;
+			payloadBytes = (byte[]) payload;
 		} else if (payload instanceof String) {
 			mimeType = MimeTypeUtils.TEXT_PLAIN_VALUE;
 			charset = DEFAULT_ENCODING;
-			attachment = ((String) payload).getBytes(charset);
+			payloadBytes = ((String) payload).getBytes(charset);
 		} else if (payload instanceof Serializable) {
 			mimeType = MIME_JAVA_SERIALIZED_OBJECT;
-			attachment = SerializationUtils.serialize(payload);
+			payloadBytes = SerializationUtils.serialize(payload);
 		} else {
 			throw new IllegalArgumentException(String.format(
 					"Invalid payload received. Expected byte[], String, or Serializable. Received: %s",
 					payload.getClass().getName()));
 		}
 
-		BytesXMLMessage xmlMessage = JCSMPFactory.onlyInstance().createMessage(BytesXMLMessage.class);
-		xmlMessage.writeAttachment(attachment);
-		xmlMessage.setHTTPContentType(mimeType);
-		if (charset != null) xmlMessage.setHTTPContentEncoding(charset.name());
-		return xmlMessage;
+		MessageWrapper messageWrapper = new MessageWrapper(message.getHeaders(), payloadBytes, mimeType);
+		if (charset != null) messageWrapper.setCharset(charset.name());
+		return messageWrapper;
 	}
 
-	public Message<?> map(XMLMessage xmlMessage) throws MessagingException {
-		byte[] attachment = xmlMessage.getAttachmentByteBuffer().array();
-		String contentType = xmlMessage.getHTTPContentType();
-		String encodingName = xmlMessage.getHTTPContentEncoding();
+	private MessageWrapper extractMessageWrapper(XMLMessage xmlMessage) {
+		String messageId = xmlMessage.getMessageId();
 
-		Object payload = null;
-		if (contentType != null) {
-			if (contentType.startsWith(MimeTypeUtils.TEXT_PLAIN.getType())) {
-				Charset encoding = StringUtils.hasText(encodingName) ? Charset.forName(encodingName) : DEFAULT_ENCODING;
-				payload = new String(attachment, encoding);
-			} else if (contentType.equalsIgnoreCase(MIME_JAVA_SERIALIZED_OBJECT)) {
-				payload = SerializationUtils.deserialize(attachment);
-			}
+		String contentType = xmlMessage.getHTTPContentType();
+		if (!contentType.equalsIgnoreCase(MIME_JAVA_SERIALIZED_OBJECT)) {
+			throw new IllegalArgumentException(String.format(
+					"Received Solace message %s with an invalid contentType header. Expected %s. Received %s",
+					messageId, MIME_JAVA_SERIALIZED_OBJECT, contentType));
 		}
 
-		return new DefaultMessageBuilderFactory().withPayload(payload != null ? payload : attachment)
-				.setHeaderIfAbsent("deliveryAttempt", new AtomicInteger(0))
-				.build();
+		byte[] attachment = xmlMessage.getAttachmentByteBuffer().array();
+		Object serializedMessage = SerializationUtils.deserialize(attachment);
+
+		if (serializedMessage == null) {
+			throw new MessagingException(String.format("Received Solace message %s with an empty attachment.",
+					messageId));
+		}
+		else if (!(serializedMessage instanceof MessageWrapper)) {
+			throw new IllegalArgumentException(String.format(
+					"Received Solace Message %s with an invalid attachment. Expected %s. Received %s",
+					messageId, MessageWrapper.class.getName(), serializedMessage.getClass().getName()));
+		}
+
+		return (MessageWrapper) serializedMessage;
+	}
+
+
+	private static class MessageWrapper implements Serializable {
+		private MessageHeaders headers;
+		private byte[] payload;
+		private String payloadMimeType;
+		private String charset;
+
+		MessageWrapper(MessageHeaders headers, byte[] payload, String payloadMimeType) {
+			this.headers = headers;
+			this.payload = payload;
+			this.payloadMimeType = payloadMimeType;
+		}
+
+		MessageHeaders getHeaders() {
+			return headers;
+		}
+
+		byte[] getPayload() {
+			return payload;
+		}
+
+		String getPayloadMimeType() {
+			return payloadMimeType;
+		}
+
+		void setCharset(String charset) {
+			this.charset = charset;
+		}
+
+		String getCharset() {
+			return charset;
+		}
 	}
 }
