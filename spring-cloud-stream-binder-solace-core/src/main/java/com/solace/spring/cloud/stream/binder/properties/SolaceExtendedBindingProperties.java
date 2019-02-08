4de668a866977e998d4442b896f91065dcae8ae9
Jeffrey Douangpaseuth <jdouangpaseuth@hotmail.ca>
2019-02-08 14:42:50 -0500

    add support for default config properties

--- spring-cloud-stream-binder-solace-core/src/main/java/com/solace/spring/cloud/stream/binder/properties/SolaceExtendedBindingProperties.java
+++ spring-cloud-stream-binder-solace-core/src/main/java/com/solace/spring/cloud/stream/binder/properties/SolaceExtendedBindingProperties.java
@@ -1,47 +1,14 @@
 package com.solace.spring.cloud.stream.binder.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.stream.binder.AbstractExtendedBindingProperties;
 import org.springframework.cloud.stream.binder.BinderSpecificPropertiesProvider;
-import org.springframework.cloud.stream.binder.ExtendedBindingProperties;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @ConfigurationProperties("spring.cloud.stream.solace")
-public class SolaceExtendedBindingProperties implements ExtendedBindingProperties<SolaceConsumerProperties,SolaceProducerProperties> {
+public class SolaceExtendedBindingProperties
+		extends AbstractExtendedBindingProperties<SolaceConsumerProperties,SolaceProducerProperties,SolaceBindingProperties> {
+
 	private static final String DEFAULTS_PREFIX = "spring.cloud.stream.solace.default";
-
-	private Map<String,SolaceBindingProperties> bindings = new HashMap<>();
-
-	@Override
-	public synchronized SolaceConsumerProperties getExtendedConsumerProperties(String channelName) {
-		if (bindings.containsKey(channelName)) {
-			if (bindings.get(channelName).getConsumer() == null) {
-				bindings.get(channelName).setConsumer(new SolaceConsumerProperties());
-			}
-		} else {
-			SolaceBindingProperties bindingProperties = new SolaceBindingProperties();
-			bindingProperties.setConsumer(new SolaceConsumerProperties());
-			bindings.put(channelName, bindingProperties);
-		}
-
-		return bindings.get(channelName).getConsumer();
-	}
-
-	@Override
-	public synchronized SolaceProducerProperties getExtendedProducerProperties(String channelName) {
-		if (bindings.containsKey(channelName)) {
-			if (bindings.get(channelName).getProducer() == null) {
-				bindings.get(channelName).setProducer(new SolaceProducerProperties());
-			}
-		} else {
-			SolaceBindingProperties bindingProperties = new SolaceBindingProperties();
-			bindingProperties.setProducer(new SolaceProducerProperties());
-			bindings.put(channelName, bindingProperties);
-		}
-
-		return bindings.get(channelName).getProducer();
-	}
 
 	@Override
 	public String getDefaultsPrefix() {
@@ -51,13 +18,5 @@ public class SolaceExtendedBindingProperties implements ExtendedBindingPropertie
 	@Override
 	public Class<? extends BinderSpecificPropertiesProvider> getExtendedPropertiesEntryClass() {
 		return SolaceBindingProperties.class;
-	}
-
-	public Map<String, SolaceBindingProperties> getBindings() {
-		return bindings;
-	}
-
-	public void setBindings(Map<String, SolaceBindingProperties> bindings) {
-		this.bindings = bindings;
 	}
 }
