71d0e7e491a9568d3d635ea5509d172cb816a6d7
Jeffrey Douangpaseuth <jdouangpaseuth@hotmail.ca>
2021-03-24 10:15:46 -0400

    cleanup everything

--- spring-cloud-stream-binder-solace-core/src/main/java/com/solace/spring/cloud/stream/binder/util/ClosedChannelBindingException.java
+++
@@ -1,7 +0,0 @@
-package com.solace.spring.cloud.stream.binder.util;
-
-public class ClosedChannelBindingException extends RuntimeException {
-	public ClosedChannelBindingException(String message) {
-		super(message);
-	}
-}
